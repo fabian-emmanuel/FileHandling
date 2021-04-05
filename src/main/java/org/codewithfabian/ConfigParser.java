d19085595a1a2981cd95207e68fe6f12596fe3ed
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-04-05 21:17:50 +0100

    Commiting task

---
+++ src/main/java/org/codewithfabian/ConfigParser.java
@@ -0,0 +1,64 @@
+package org.codewithfabian;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class ConfigParser {
+    private final Map<String, String> readFile;
+    private final String filePath;
+    Scanner scReader;
+
+    public ConfigParser(File configFilename){
+        readFile = new LinkedHashMap<>();
+        this.filePath = "src/main/java/resources/"+configFilename;
+    }
+
+    private void parseConfigFile() {
+        try {
+            FileReader reader = new FileReader(filePath);
+            scReader = new Scanner(reader);
+
+            while(scReader.hasNextLine()){
+                String data = scReader.nextLine();
+
+                if(data.contains("=")){
+                    int counter = 0;
+                    String[] arrStr = data.split("=");
+                    String key = arrStr[0];
+                    String value = arrStr[1];
+
+                    if(readFile.size() != 0){
+                        for(Map.Entry<String, String> items: readFile.entrySet()){
+                            if(key.equals("name")) key = "application.name";
+                            if(items.getKey().equals(key)) counter++;
+                        }
+                    }
+
+                    if(counter == 0){
+                        String name = "name";
+                        if(key.equals(name)) readFile.put("application.name", value);
+                        else readFile.put(key, value);
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            scReader.close();
+        }
+    }
+
+    public String get(String key){
+        this.parseConfigFile();
+        String value = null;
+
+        for(Map.Entry<String, String> item: readFile.entrySet()){
+            if(item.getKey().equals(key)) value = item.getValue();
+        }
+        return value;
+    }
+
+}
---
+++ src/main/java/org/codewithfabian/Main.java
@@ -0,0 +1,29 @@
+package org.codewithfabian;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        String myKey = "application.name";
+
+        try {
+            String fileName = args[0];
+            String filePathName = null;
+
+            if (fileName.equalsIgnoreCase("Production")) filePathName = "config.txt";
+            else if (fileName.equalsIgnoreCase("Development")) filePathName = "config.txt.dev";
+            else if (fileName.equalsIgnoreCase("Staging")) filePathName = "config.txt.staging";
+            else System.out.println("Invalid argument!");
+
+            assert filePathName != null;
+            File configFile = new File(filePathName);
+            ConfigParser config = new ConfigParser(configFile);
+            System.out.println(config.get(myKey));
+
+        } catch (ArrayIndexOutOfBoundsException a){
+            File configFile = new File("config.txt");
+            ConfigParser config = new ConfigParser(configFile);
+            System.out.println(config.get(myKey));
+        }
+    }
+}
