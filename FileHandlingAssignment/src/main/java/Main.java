37255cfeb412a7ebcf5b0fe8d28e5fe3343cca08
FABIAN EMMANUEL <fabulousval4ril@gmail.com>
2021-04-09 17:00:23 +0100

    Add files via upload

--- FileHandlingAssignment/src/main/java/Main.java
+++ FileHandlingAssignment/src/main/java/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         String filePath = null;
+        String absolutePath = "/Users/decagon/FileHandlingAssignment/src/main/resources/";
 
-        if(args.length == 0 || args[0].equals("production")) filePath = "src/main/resources/config.txt";
-        else if(args[0].equalsIgnoreCase("development")) filePath = "src/main/resources/config.txt.dev";
-        else if(args[0].equalsIgnoreCase("staging")) filePath = "src/main/resources/config.txt.staging";
+        if(args.length == 0 || args[0].equals("production")) filePath = absolutePath+"config.txt";
+        else if(args[0].equalsIgnoreCase("development")) filePath = absolutePath+"config.txt.dev";
+        else if(args[0].equalsIgnoreCase("staging")) filePath = absolutePath+"config.txt.staging";
         else System.out.println("Invalid Argument");
 
-        assert filePath != null;
         ConfigParser config = new ConfigParser();
 
         String valueOfDbname = config.configParser(filePath).get("dbname");
@@ -16,8 +16,10 @@ public class Main {
         String valueOfMode = config.configParser(filePath).get("mode");
         String valueOfTheme = config.configParser(filePath).get("theme");
         String valueOfPipeline = config.configParser(filePath).get("pipeline");
+
         System.out.println(valueOfDbname + " ** " + valueOfHost + " ** " + valueOfMode + " ** " + valueOfTheme + " ** " + valueOfPipeline );
 
+        System.out.println("************************");
 
         String valueOfName = config.configParser(filePath).get("application.name");
         String valueOfPort = config.configParser(filePath).get("application.port");
--- FileHandlingAssignment/src/test/java/ConfigParserTest.java
+++ FileHandlingAssignment/src/test/java/ConfigParserTest.java
@@ -6,9 +6,24 @@ public class ConfigParserTest {
 
     @Test
     void getTest() {
-        String filePath = "src/main/resources/config.txt/";
+        String filePath = "/Users/decagon/FileHandlingAssignment/src/main/resources/config.txt";
         ConfigParser config = new ConfigParser();
         String expected = "production";
-        assertEquals(expected, config.configParser(filePath).get("application.name"));
+        assertEquals(expected, config.configParser(filePath).get("mode"));
+    }
+        @Test
+        void getTest2 () {
+            String filePath2 = "/Users/decagon/FileHandlingAssignment/src/main/resources/config.txt.dev";
+            ConfigParser config2 = new ConfigParser();
+            String expected2 = "development";
+            assertEquals(expected2, config2.configParser(filePath2).get("mode"));
+        }
+        @Test
+        void getTest3(){
+
+        String filePath3 = "/Users/decagon/FileHandlingAssignment/src/main/resources/config.txt.staging";
+        ConfigParser config3 = new ConfigParser();
+        String expected3 = "staging";
+        assertEquals(expected3, config3.configParser(filePath3).get("mode"));
     }
 }
